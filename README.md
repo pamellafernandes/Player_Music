7b21a2249d92d37d27d4b26102f5cc43dee3b501
pdlima <pdlima@CPQI-NBK-92>
2022-03-06 21:18:40 -0300

    Player Music

---
+++ README.md
@@ -0,0 +1,42 @@
+<h1 align="center">Player Music</h1>
+
+## Descrição do Projeto
+
+<p align="center" style="font-size: 1.2rem;">Esse projeto é baseado em um reprodutor de música.</p>
+
+### Features
+
+- [x] Play da música
+- [x] Pause da música
+- [x] Retorna a música anterior
+- [x] Avança a música anterior
+
+### Skills
+
+<div>
+<img src="https://img.shields.io/badge/HTML5-E34F26?style=for-the-badge&logo=html5&logoColor=white">
+
+<img src="https://img.shields.io/badge/CSS-1e79e2?&style=for-the-badge&logo=css3&logoColor=white">
+
+<img src="https://img.shields.io/badge/JavaScript-F7DF1E?style=for-the-badge&logo=javascript&logoColor=black">
+</div>
+
+### Inspiração
+
+<p> O projeto foi inspirado em um reprodutor de música. Compartilho este conhecimento através deste vídeo feito pelo <a href="https://www.youtube.com/watch?v=gFP7A_umApE&list=PLJ8PYFcmwFOxmqYNlo_H8TYVSDLxB8HdR&index=2">João Tinti</a></p>
+
+<h2 align="center" style="font-size: 1.2rem;"> 
+	Projeto concluído com sucesso... 🚀
+</h2>
+
+### Resultado
+
+Para ver o resultado deste projeto, basta clonar o projeto as instruções estão abaixo: ⤵️
+
+    <h2 align="left">
+        Clone o projeto da seguinte forma:
+    </h2>
+
+```sh
+git clone https://github.com/pamellafernandes/Player_Music
+```
---
+++ script/script.js
@@ -0,0 +1,98 @@
+let musicas = [
+  {
+    titulo: "Do I Wanna Know?",
+    artista: "Arctic Monkeys",
+    src: "music/01_Do_I_Wanna_Know.mp3",
+    img: "img/arctic.jpg",
+  },
+  {
+    titulo: "Sweer Child O' Mine",
+    artista: "Guns N' Roses",
+    src: "music/Sweet Child O' Mine.mp3",
+    img: "img/guns.jpg",
+  },
+  {
+    titulo: "Back In Black",
+    artista: "AC/DC",
+    src: "music/ACDC - Back In Black (Official Music Video).mp3",
+    img: "img/ac_dc.jpg",
+  },
+];
+
+let musica = document.querySelector("audio");
+let indexMusica = 0;
+
+let duracaoMusica = document.querySelector(".fim");
+let imagem = document.querySelector("img");
+let nomeMusica = document.querySelector(".descricao h2");
+let nomeArtista = document.querySelector(".descricao i");
+
+renderizarMusica(indexMusica);
+
+// Eventos
+document.querySelector(".botao-play").addEventListener("click", tocarMusica);
+
+document.querySelector(".botao-pause").addEventListener("click", pausarMusica);
+
+musica.addEventListener("timeupdate", atualizarBarra);
+
+document.querySelector(".anterior").addEventListener("click", () => {
+  indexMusica--;
+  if (indexMusica < 0) {
+    indexMusica = 2;
+  }
+  renderizarMusica(indexMusica);
+});
+
+document.querySelector(".proxima").addEventListener("click", () => {
+  indexMusica++;
+  if (indexMusica > 2) {
+    indexMusica = 0;
+  }
+  renderizarMusica(indexMusica);
+});
+
+// Funções
+function renderizarMusica(index) {
+  musica.setAttribute("src", musicas[index].src);
+  musica.addEventListener("loadeddata", () => {
+    nomeMusica.textContent = musicas[index].titulo;
+    nomeArtista.textContent = musicas[index].artista;
+    imagem.src = musicas[index].img;
+    duracaoMusica.textContent = segundosParaMinutos(
+      Math.floor(musica.duration)
+    );
+  });
+}
+
+function tocarMusica() {
+  musica.play();
+  document.querySelector(".botao-pause").style.display = "block";
+  document.querySelector(".botao-play").style.display = "none";
+}
+
+function pausarMusica() {
+  musica.pause();
+  document.querySelector(".botao-pause").style.display = "none";
+  document.querySelector(".botao-play").style.display = "block";
+}
+
+function atualizarBarra() {
+  let barra = document.querySelector("progress");
+  barra.style.width =
+    Math.floor((musica.currentTime / musica.duration) * 100) + "%";
+  let tempoDecorrido = document.querySelector(".inicio");
+  tempoDecorrido.textContent = segundosParaMinutos(
+    Math.floor(musica.currentTime)
+  );
+}
+
+function segundosParaMinutos(segundos) {
+  let campoMinutos = Math.floor(segundos / 60);
+  let campoSegundos = segundos % 60;
+  if (campoSegundos < 10) {
+    campoSegundos = "0" + campoSegundos;
+  }
+
+  return campoMinutos + ":" + campoSegundos;
+}
